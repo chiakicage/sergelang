a4e532df3773f7586387aada98dba0acaf405a31
chiakicage <colorful_cache@outlook.com>
2023-05-28 07:15:03 +0800

    [midend] added func mir generate

--- src/midend/mir.rs
+++ src/midend/mir.rs
@@ -1,14 +1,13 @@
 /// The basic design of middle level IR - MIR, follows the MIR of rust
-
 use slotmap::{new_key_type, SlotMap};
 use std::collections::HashSet;
 use std::fmt::Display;
 use std::fmt::Write;
 use std::{panic, vec};
 
+use crate::ast::ast::{BinOp, UnOp};
 use crate::midend::typed_ast::*;
 use crate::utils::type_context::*;
-use crate::ast::ast::{BinOp, UnOp};
 
 use rpds::HashTrieMap;
 
@@ -29,14 +28,18 @@ pub struct Func {
     pub variables: SlotMap<VarRef, Var>,
 
     pub params: Vec<VarRef>,
+    pub captured: Vec<VarRef>,
 
     // corresponds to MIR Locals
-    pub locals: Vec<VarRef>, 
+    pub locals: Vec<VarRef>,
     // Intermediate Rvalues
-    pub temporaries: Vec<VarRef>, 
+    pub temporaries: Vec<VarRef>,
 
-    pub return_value: Option<VarRef>, 
+    pub return_value: Option<VarRef>,
 
+    pub entry: BlockRef,
+    pub exit: BlockRef,
+    pub panic: BlockRef,
 }
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
@@ -56,9 +59,8 @@ pub struct Block {
 #[derive(Debug, Default, Clone)]
 pub struct Stmt {
     pub left: Option<VarRef>,
-    pub right: Option<Rvalue>
+    pub right: Option<Rvalue>,
 }
-
 
 // MIR Terminator
 #[derive(Debug, Clone)]
@@ -73,7 +75,7 @@ pub enum Terminator {
 #[derive(Debug, Clone)]
 pub struct Operand {
     pub typ: TypeRef,
-    pub val: Box<OperandEnum>,
+    pub val: OperandEnum,
 }
 
 #[derive(Debug, Clone)]
@@ -87,15 +89,86 @@ pub enum OperandEnum {
 pub enum Rvalue {
     BinaryOperator(BinOp, Operand, Operand),
     UnaryOperator(UnOp, Operand, Operand),
-    Call(String, Vec<Operand>)
+    Call(String, Vec<Operand>),
 }
-
-
 
 #[derive(Debug, Default, Clone)]
 pub struct MIR {
     pub ty_ctx: TypeContext,
     pub name_ctx: SymTable<String, TypeRef>,
-    pub module: Vec<Func>
+    pub module: Vec<Func>,
 }
 
+impl MIR {
+    pub fn create_from_typed_ast(typed_ast: &TypedModule) -> Self {
+        let ty_ctx = typed_ast.ty_ctx.clone();
+        let name_ctx = typed_ast.func_table.clone();
+        let mut mir = MIR {
+            ty_ctx,
+            name_ctx,
+            module: Vec::new(),
+        };
+        for func in &typed_ast.func_defs {
+            mir.module
+                .push(mir.build_func(func, mir.name_ctx.clone(), SymTable::new()));
+        }
+        mir
+    }
+    fn build_func(
+        &self,
+        func: &TypedFunc,
+        ext_name_ctx: SymTable<String, TypeRef>,
+        name_var_map: SymTable<String, VarRef>,
+    ) -> Func {
+        let mut blocks = SlotMap::with_key();
+        let exit = blocks.insert(Block {
+            name: "exit".to_string(),
+            stmts: Vec::new(),
+            terminator: Terminator::Return,
+        });
+        let entry = blocks.insert(Block {
+            name: "entry".to_string(),
+            stmts: Vec::new(),
+            terminator: Terminator::Jump(exit),
+        });
+        let panic = blocks.insert(Block {
+            name: "panic".to_string(),
+            stmts: Vec::new(),
+            terminator: Terminator::Panic,
+        });
+
+        let mut variables = SlotMap::with_key();
+        let mut name_ctx = ext_name_ctx.clone();
+        let mut name_var_map = name_var_map.clone();
+        let params = func
+            .params
+            .iter()
+            .map(|param| {
+                let var = Var {
+                    name: param.0.clone(),
+                    typ: param.1,
+                };
+                let var = variables.insert(var);
+                name_ctx = name_ctx.insert(param.0.clone(), param.1);
+                name_var_map = name_var_map.insert(param.0.clone(), var);
+                var
+            })
+            .collect::<Vec<_>>();
+
+        let mut func = Func {
+            name: func.name.clone(),
+            typ: func.ty,
+            blocks,
+            variables,
+            params,
+            captured: Vec::new(),
+            locals: Vec::new(),
+            temporaries: Vec::new(),
+            return_value: None,
+            entry,
+            exit,
+            panic,
+        };
+        func
+    }
+}
