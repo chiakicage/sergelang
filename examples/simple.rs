6d7ce9f7449099c25ba5039166302496334a680c
chiakicage <colorful_cache@outlook.com>
2023-05-29 18:28:11 +0800

    [midend] allow any type in println

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,6 +1,15 @@
+enum List {
+	Nil,
+	Cons(i32, List)
+}
 fn __serge_user_main() -> i32 {
 	let a: i32 = __serge_read_i32();
-	let b: i32 = a + 1;
-	__serge_println(10 + 10);
+	let b: i32 = __serge_read_i32();
+	let c: i32 = a + b;
+	// let list: List = List::Cons(1, List::Cons(2, List::Nil));
+	let e: f64 = 1.0;
+	__serge_println(c);
+	__serge_println(e);
+	// __serge_println(list);
 	0
 }
