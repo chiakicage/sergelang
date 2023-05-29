2798f5f025bff7bee1fa421166d6426fab7e7b42
chiakicage <colorful_cache@outlook.com>
2023-05-30 05:05:49 +0800

    [midend] added array

--- src/backend/runtime.rs
+++ src/backend/runtime.rs
@@ -162,8 +162,8 @@ impl<'a> RuntimeLibrary<'a> for CodeGen<'a> {
             // array
             insert_runtime_function!(false; alloc_array: | | => ptr_type);
             insert_runtime_function!(false; array_length: |  | => int_type);
-            insert_runtime_function!(false; array_index: | ptr_type, int_type | => ptr_type);
-            insert_runtime_function!(false; array_write_index: | ptr_type, int_type, ptr_type| => void_type);
+            insert_runtime_function!(false; array_index: | ptr_type, ptr_type | => ptr_type);
+            insert_runtime_function!(false; array_write_index: | ptr_type, ptr_type, ptr_type| => void_type);
             insert_runtime_function!(false; array_push_back: | ptr_type, ptr_type | => void_type);
             // tuple
             insert_runtime_function!(true; make_tuple: | int_type | => void_type);
